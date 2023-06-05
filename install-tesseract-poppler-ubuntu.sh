78490ffa6bd837e6d075745081e944c93c4a0da9
cthiounn2 <thiounn.conrad@gmail.com>
2023-06-05 11:32:58 +0000

    shorter install script

--- install-tesseract-poppler-ubuntu.sh
+++ install-tesseract-poppler-ubuntu.sh
@@ -1,7 +1,3 @@
-sudo apt-get install -y apt-transport-https
-sudo echo "deb https://notesalexp.org/tesseract-ocr5/jammy/ jammy main" >> /etc/apt/sources.list
-sudo apt-get update -oAcquire::AllowInsecureRepositories=true
-sudo apt-get install -y notesalexp-keyring -oAcquire::AllowInsecureRepositories=true
 sudo apt-get update
 sudo apt install -y tesseract-ocr-all
 sudo apt install -y poppler-utils
